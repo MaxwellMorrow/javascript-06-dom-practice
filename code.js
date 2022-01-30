2b028b139ff69053482535335e13200a20866030
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-01-30 11:24:36 -0800

    finished activity

--- code.js
+++ code.js
@@ -1,9 +1,21 @@
+
+
+
 // 1. change h1 text content to your name
-
+let h1 = document.querySelector("h1")
+h1.textContent = "Max"
 // 2. change the body background color to any color
-
+let body = document.querySelector("body")
+body.style.backgroundColor = "blue"
 // 3. change content with a 'quote' class to bold font
-
+let quote = document.querySelector(".quote")
+quote.style.fontWeight = "bold"
 // 4. hide content with the id of 'hideMe' (hint: explore the css property 'display')
-
+let hideMe = document.querySelector("#hideMe")
+hideMe.style.display = "none"
 // 5. hide all content with the id of 'hideAll' (note: querySelectorAll returns an array, you will need to access each index in the array)
+let hideAll = document.querySelectorAll("#hideAll")
+hideAll.forEach((elem)=>{
+ elem.style.display = "none"
+
+})
